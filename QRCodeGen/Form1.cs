0fe1fb254c3230f9eb517048a38b3291469ac0d1
Darryl Bordelon <erroch@gmail.com>
2025-08-11 20:42:14 -0500

    remove attempting to lead SVG icons.

--- QRCodeGen/Form1.cs
+++ QRCodeGen/Form1.cs
@@ -172,7 +172,7 @@ namespace QRCodeGen
 
         private void btnUploadIcon_Click(object sender, EventArgs e)
         {
-            this.openFileDialog1.Filter = "Bitmap Image|*.bmp|PNG Image|*.png|SVG Imagee|*.svg";
+            this.openFileDialog1.Filter = "Bitmap Image|*.bmp|PNG Image|*.png";
             DialogResult result = this.openFileDialog1.ShowDialog();
 
             if (result == DialogResult.OK)
