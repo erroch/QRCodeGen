71c5984758fe19c7177f2830962e90164d18323b
Darryl Bordelon <erroch@gmail.com>
2025-08-11 20:10:19 -0500

    Add project files.

---
+++ QRCodeGen/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace QRCodeGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtURL = new TextBox();
+            label2 = new Label();
+            btnUploadIcon = new Button();
+            picQRCode = new PictureBox();
+            btnSave = new Button();
+            btnGenerate = new Button();
+            saveFileDialog1 = new SaveFileDialog();
+            label3 = new Label();
+            cboECCLevel = new ComboBox();
+            openFileDialog1 = new OpenFileDialog();
+            numericUpDownIconSize = new NumericUpDown();
+            picIcon = new PictureBox();
+            btnClearIcon = new Button();
+            panel1 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)picQRCode).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownIconSize).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)picIcon).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(25, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 0;
+            label1.Text = "URL:";
+            // 
+            // txtURL
+            // 
+            txtURL.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtURL.Location = new Point(63, 6);
+            txtURL.Name = "txtURL";
+            txtURL.Size = new Size(394, 23);
+            txtURL.TabIndex = 1;
+            txtURL.TextChanged += txtURL_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Icon %:";
+            // 
+            // btnUploadIcon
+            // 
+            btnUploadIcon.Location = new Point(211, 38);
+            btnUploadIcon.Name = "btnUploadIcon";
+            btnUploadIcon.Size = new Size(75, 23);
+            btnUploadIcon.TabIndex = 3;
+            btnUploadIcon.Text = "Set Icon";
+            btnUploadIcon.UseVisualStyleBackColor = true;
+            btnUploadIcon.Click += btnUploadIcon_Click;
+            // 
+            // picQRCode
+            // 
+            picQRCode.Location = new Point(3, 3);
+            picQRCode.Name = "picQRCode";
+            picQRCode.Size = new Size(256, 256);
+            picQRCode.TabIndex = 5;
+            picQRCode.TabStop = false;
+            // 
+            // btnSave
+            // 
+            btnSave.Location = new Point(4, 128);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(75, 23);
+            btnSave.TabIndex = 6;
+            btnSave.Text = "Save";
+            btnSave.UseVisualStyleBackColor = true;
+            btnSave.Click += btnSave_Click;
+            // 
+            // btnGenerate
+            // 
+            btnGenerate.Location = new Point(4, 99);
+            btnGenerate.Name = "btnGenerate";
+            btnGenerate.Size = new Size(75, 23);
+            btnGenerate.TabIndex = 7;
+            btnGenerate.Text = "Generate";
+            btnGenerate.UseVisualStyleBackColor = true;
+            btnGenerate.Click += btnGenerate_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(25, 38);
+            label3.Name = "label3";
+            label3.Size = new Size(32, 15);
+            label3.TabIndex = 8;
+            label3.Text = "ECC:";
+            // 
+            // cboECCLevel
+            // 
+            cboECCLevel.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboECCLevel.FormattingEnabled = true;
+            cboECCLevel.Items.AddRange(new object[] { "Default", "L", "M", "Q", "H" });
+            cboECCLevel.Location = new Point(63, 35);
+            cboECCLevel.Name = "cboECCLevel";
+            cboECCLevel.Size = new Size(121, 23);
+            cboECCLevel.TabIndex = 9;
+            cboECCLevel.SelectedIndexChanged += cboECCLevel_SelectedIndexChanged;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // numericUpDownIconSize
+            // 
+            numericUpDownIconSize.Location = new Point(63, 66);
+            numericUpDownIconSize.Name = "numericUpDownIconSize";
+            numericUpDownIconSize.Size = new Size(120, 23);
+            numericUpDownIconSize.TabIndex = 11;
+            numericUpDownIconSize.Value = new decimal(new int[] { 16, 0, 0, 0 });
+            // 
+            // picIcon
+            // 
+            picIcon.Location = new Point(292, 35);
+            picIcon.Name = "picIcon";
+            picIcon.Size = new Size(64, 64);
+            picIcon.TabIndex = 12;
+            picIcon.TabStop = false;
+            // 
+            // btnClearIcon
+            // 
+            btnClearIcon.Location = new Point(211, 64);
+            btnClearIcon.Name = "btnClearIcon";
+            btnClearIcon.Size = new Size(75, 23);
+            btnClearIcon.TabIndex = 13;
+            btnClearIcon.Text = "Clear Icon";
+            btnClearIcon.UseVisualStyleBackColor = true;
+            btnClearIcon.Click += btnClearIcon_Click;
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.AutoScroll = true;
+            panel1.Controls.Add(picQRCode);
+            panel1.Location = new Point(86, 105);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(375, 366);
+            panel1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(469, 478);
+            Controls.Add(panel1);
+            Controls.Add(btnClearIcon);
+            Controls.Add(picIcon);
+            Controls.Add(numericUpDownIconSize);
+            Controls.Add(cboECCLevel);
+            Controls.Add(label3);
+            Controls.Add(btnGenerate);
+            Controls.Add(btnSave);
+            Controls.Add(btnUploadIcon);
+            Controls.Add(label2);
+            Controls.Add(txtURL);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "QR Code Generator";
+            ((System.ComponentModel.ISupportInitialize)picQRCode).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDownIconSize).EndInit();
+            ((System.ComponentModel.ISupportInitialize)picIcon).EndInit();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtURL;
+        private Label label2;
+        private Button btnUploadIcon;
+        private PictureBox picQRCode;
+        private Button btnSave;
+        private Button btnGenerate;
+        private SaveFileDialog saveFileDialog1;
+        private Label label3;
+        private ComboBox cboECCLevel;
+        private OpenFileDialog openFileDialog1;
+        private NumericUpDown numericUpDownIconSize;
+        private PictureBox picIcon;
+        private Button btnClearIcon;
+        private Panel panel1;
+    }
+}
---
+++ QRCodeGen/Form1.cs
@@ -0,0 +1,222 @@
+using QRCoder;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace QRCodeGen
+{
+    public partial class Form1 : Form
+    {
+        private QRCodeGenerator.ECCLevel eccLevel = QRCodeGenerator.ECCLevel.Default;
+        private Bitmap? iconImage = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.cboECCLevel.SelectedIndex = 0;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            this.PreviewCode();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            this.saveFileDialog1.Filter = "Bitmap Image|*.bmp|PNG Image|*.png|SVG Imagee|*.svg";
+            this.saveFileDialog1.Title = "Save QR Code";
+            this.saveFileDialog1.ShowDialog();
+
+            if (this.saveFileDialog1.FileName != "")
+            {
+                if (this.saveFileDialog1.FileName.EndsWith(".svg"))
+                {
+                    var qrGenerator = new QRCodeGenerator();
+                    var codeData = qrGenerator.CreateQrCode(this.txtURL.Text.Trim(), this.eccLevel);
+                    var svgImage = new SvgQRCode(codeData);
+                    try
+                    {
+                        string svgString = svgImage.GetGraphic(10);
+                        System.IO.FileStream fs = (System.IO.FileStream)this.saveFileDialog1.OpenFile();
+                        using (StreamWriter outputFile = new StreamWriter(fs))
+                        {
+                            outputFile.Write(svgString);
+                            outputFile.Close();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(
+                            $"Error Saving SVG Image: {ex.Message}",
+                            "Error Saving Image",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    var qrGenerator = new QRCodeGenerator();
+                    var codeData = qrGenerator.CreateQrCode(this.txtURL.Text.Trim(), this.eccLevel);
+                    var qrCodeBitmap = new QRCode(codeData).GetGraphic(
+                        pixelsPerModule: 10,
+                        darkColor: Color.Black,
+                        lightColor: Color.White,
+                        icon: this.iconImage,
+                        iconSizePercent: Convert.ToInt32(this.numericUpDownIconSize.Value),
+                        iconBackgroundColor: Color.White);
+                    try
+                    {
+                        ImageFormat saveFormat = ImageFormat.Bmp;
+
+                        switch (this.saveFileDialog1.FilterIndex)
+                        {
+                            case 1:
+                                saveFormat = ImageFormat.Bmp;
+                                break;
+                            case 2:
+                                saveFormat = ImageFormat.Png;
+                                break;
+                        }
+
+                        System.IO.FileStream fs = (System.IO.FileStream)this.saveFileDialog1.OpenFile();
+                        qrCodeBitmap.Save(fs, saveFormat);
+                        fs.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(
+                            $"Error Saving BMP Image: {ex.Message}",
+                            "Error Saving Image",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                }
+            }
+        }
+
+        private void PreviewCode()
+        {
+            if (txtURL.Text.Trim().Length > 0)
+            {
+                var qrGenerator = new QRCodeGenerator();
+                var codeData = qrGenerator.CreateQrCode(this.txtURL.Text.Trim(), this.eccLevel);
+                var qrCodeBitmap = new QRCode(codeData)
+                    .GetGraphic(
+                        pixelsPerModule: 10,
+                        darkColor: Color.Black,
+                        lightColor: Color.White,
+                        icon: this.iconImage,
+                        iconSizePercent: Convert.ToInt32(this.numericUpDownIconSize.Value),
+                        iconBackgroundColor: Color.White);
+                this.picQRCode.Width = qrCodeBitmap.Width;
+                this.picQRCode.Height = qrCodeBitmap.Height;
+                this.picQRCode.Image = qrCodeBitmap;
+            }
+        }
+
+        private void txtURL_TextChanged(object sender, EventArgs e)
+        {
+            this.PreviewCode();
+        }
+
+        private void cboECCLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.cboECCLevel.SelectedItem != null)
+            {
+                switch ((string)this.cboECCLevel.SelectedItem)
+                {
+                    case "Default":
+                        this.eccLevel = QRCodeGenerator.ECCLevel.Default;
+                        break;
+                    case "L":
+                        this.eccLevel = QRCodeGenerator.ECCLevel.L;
+                        break;
+                    case "M":
+                        this.eccLevel = QRCodeGenerator.ECCLevel.M;
+                        break;
+                    case "Q":
+                        this.eccLevel = QRCodeGenerator.ECCLevel.Q;
+                        break;
+                    case "H":
+                        this.eccLevel = QRCodeGenerator.ECCLevel.H;
+                        break;
+                    default:
+                        this.eccLevel = QRCodeGenerator.ECCLevel.Default;
+                        break;
+                }
+            }
+            else
+            {
+                this.eccLevel = QRCodeGenerator.ECCLevel.Default;
+            }
+
+            this.PreviewCode();
+        }
+
+        private Bitmap? GenerateIconImage()
+        {
+            if (this.iconImage == null)
+                return null;
+
+            int iconSize = Convert.ToInt32(Math.Round(this.numericUpDownIconSize.Value, 0));
+            Bitmap result = new Bitmap(iconSize, iconSize);
+            Graphics graphics = Graphics.FromImage(result);
+            graphics.CompositingMode = CompositingMode.SourceCopy;
+            graphics.CompositingQuality = CompositingQuality.HighQuality;
+            graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            using (ImageAttributes imageAttributes = new ImageAttributes())
+            {
+                imageAttributes.SetWrapMode((WrapMode.TileFlipXY));
+                Rectangle targetSize = new Rectangle(0, 0, iconSize, iconSize);
+                graphics.DrawImage(this.iconImage, targetSize, 0, 0, this.iconImage.Width,
+                    this.iconImage.Height, GraphicsUnit.Pixel, imageAttributes);
+                return result;
+            }
+        }
+
+        private void btnUploadIcon_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.Filter = "Bitmap Image|*.bmp|PNG Image|*.png|SVG Imagee|*.svg";
+            DialogResult result = this.openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                try
+                {
+                    this.iconImage = new Bitmap(this.openFileDialog1.FileName);
+                    Bitmap previewImage = new Bitmap(this.picIcon.Width, this.picIcon.Height);
+                    Graphics graphics = Graphics.FromImage(previewImage);
+                    graphics.CompositingMode = CompositingMode.SourceCopy;
+                    graphics.CompositingQuality = CompositingQuality.HighQuality;
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.SmoothingMode = SmoothingMode.HighQuality;
+                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                    using (ImageAttributes imageAttributes = new ImageAttributes())
+                    {
+                        imageAttributes.SetWrapMode((WrapMode.TileFlipXY));
+                        Rectangle targetSize = new Rectangle(0, 0, 64, 64);
+                        graphics.DrawImage(this.iconImage, targetSize, 0, 0, this.iconImage.Width,
+                            this.iconImage.Height, GraphicsUnit.Pixel, imageAttributes);
+                        this.picIcon.Image = previewImage;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(
+                        $"Error Saving Loading Icon: {ex.Message}",
+                        "Error Loading Icon",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+
+        private void btnClearIcon_Click(object sender, EventArgs e)
+        {
+            this.picIcon.Image = null;
+            this.iconImage = null;
+        }
+    }
+}
